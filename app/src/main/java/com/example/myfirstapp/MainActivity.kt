a329824fd69e6c62a82dd750bf92aaebf019c8b7
Jitesh8260 <jiteshbhakat16@gmail.com>
2024-07-06 13:36:06 +0530

    Finished Layouts

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -4,19 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.horizontalScroll
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.myfirstapp.ui.theme.MyFirstAppTheme
 
@@ -29,44 +27,23 @@ class MainActivity : ComponentActivity() {
                 val purple = Color(0xFF800080)
 
                 val gradientColors = listOf(Color.Cyan, lightBlue, purple, Color.Yellow)
-
-                Column (
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(Color.Cyan),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ){
-                    for(i in 1 ..100)
-                        Text(text = "COLUMN MODE",
-                            color = Color.Red,
-                            fontSize = 25.sp,
-                            fontFamily = FontFamily.Serif,
-                            fontStyle = FontStyle.Italic,
-                            textAlign = TextAlign.Center,
-                        )
-
+                Column (modifier = Modifier
+                    .padding(24.dp)
+                    .background(Color.Black)
+                    .fillMaxWidth(.9f)
+                    .size(width = 400.dp , height = 600.dp)){
+                    Text(
+                        text = "WELCOME TO MY FIRST APP",
+                        color = Color.White,
+                        fontSize = 25.sp,
+                        fontFamily = FontFamily.Serif,
+                        fontStyle = FontStyle.Italic,
+                        textAlign = TextAlign.Center,
+                    )
                 }
-                Row (
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(Color.Cyan)
-                        .horizontalScroll(rememberScrollState()),
-                    horizontalArrangement = Arrangement.Center,
-                    verticalAlignment = Alignment.CenterVertically,
-                ){
-                    for(i in 1 ..5)
-                        Text(text = "ROW MODE",
-                            color = Color.Red,
-                            fontSize = 25.sp,
-                            fontFamily = FontFamily.Serif,
-                            fontStyle = FontStyle.Italic,
-                            textAlign = TextAlign.Center,
-                        )
                 }
             }
         }
-    }
 
     override fun onStart() {
         super.onStart()
